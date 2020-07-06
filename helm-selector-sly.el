5c6f6409d0cd3beb6db69f0d9789cd59d4de2b96
Pierre Neidhardt <mail@ambrevar.xyz>
2020-07-06 18:33:05 +0200

    SLY: Init.

---
+++ helm-selector-sly.el
@@ -0,0 +1,60 @@
+;;; helm-selector-sly.el --- Helm SLY buffer selector -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 Pierre Neidhardt
+
+;; Author: Pierre Neidhardt <mail@ambrevar.xyz>
+;; Maintainer: Pierre Neidhardt <mail@ambrevar.xyz>
+;; URL: https://github.com/emacs-helm/helm-selector
+;; Version: 0.1
+;; Package-Requires: ((emacs "25") (helm "3"))
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; <https://www.gnu.org/licenses/>
+
+;;; Code:
+
+(require 'helm-selector)
+(require 'sly nil :noerror)
+(require 'helm-sly nil :noerror)
+
+(declare-function sly "sly")
+(declare-function sly-current-connection "sly")
+(declare-function sly-mrepl--find-buffer "sly-mrepl")
+(declare-function helm-sly-mini "helm-sly")
+
+;;;###autoload
+(defun helm-selector-sly ()
+  "Helm for `sly' buffers."
+  (interactive)
+  (helm-selector
+   "SLY-REPL"
+   :predicate (lambda (buffer)
+                (eq buffer (ignore-errors
+                             (sly-mrepl--find-buffer (sly-current-connection)))))
+   :make-buffer-fn #'sly
+   :helm-sources #'helm-sly-mini))
+
+;;;###autoload
+(defun helm-selector-sly-other-window ()
+  "Like `helm-selector-sly' but raise buffer in other window."
+  (interactive)
+  (let ((current-prefix-arg t))
+    (call-interactively #'helm-selector-sly)))
+
+(provide 'helm-selector-sly)
+;;; helm-selector-sly.el ends here
